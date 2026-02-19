6fdac1629059919c765e066b396c41c8753f85bd
Cammy Anderson <canderson9756@gmail.com>
2026-02-19 13:14:39 +0000

    Implimented position methods

--- src/core/position.py
+++ src/core/position.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from .direction import Direction
+from .constants import FILE_TO_LETTER, LETTER_TO_FILE
 
 @dataclass(frozen=True)
 class Position:
@@ -7,3 +9,13 @@ class Position:
 
     def is_valid(self) -> bool:
         return 0<=self.file<=7 and 0<=self.rank<=7
+    
+    def to_algebraic(self) -> str:
+        return FILE_TO_LETTER[self.file] + str(self.rank + 1)
+    
+    @classmethod
+    def from_algebraic(cls, notation: str) -> 'Position':
+        return cls(LETTER_TO_FILE[notation[0]], int(notation[1])-1)
+
+    def __add__(self, direction: 'Direction') -> 'Position':
+        return Position(self.file+direction.dx, self.rank+direction.dy)
--- tests/test_core/test_position.py
+++ tests/test_core/test_position.py
@@ -1,4 +1,5 @@
 from src.core.position import Position
+from src.core.direction import Direction
 import pytest
 
 def test_position_has_rank_and_file():
@@ -26,3 +27,39 @@ def test_position_equality_by_value():
 
 def test_position_is_valid(file,rank,expected):
     assert Position(file, rank).is_valid() == expected
+
+## Adding direcitons
+
+def test_position_add_direction():
+    p = Position(0,0)
+    d = Direction(0,1)
+    assert p + d == Position(0,1)
+
+def test_position_add_direction_can_go_off_board():
+    p = Position(0,0)
+    d = Direction(0,-1)
+    result = p + d
+    assert result == Position(0,-1)
+    assert not result.is_valid()
+
+## Algebraic notation
+
+@pytest.mark.parametrize("file,rank,expected", [
+     (0, 0, 'a1'),
+     (7, 7, 'h8'),
+     (5, 5, 'f6'),
+     (3, 2, 'd3')
+     ])
+
+def test_to_algebraic(file,rank,expected):
+    assert Position(file, rank).to_algebraic() == expected
+
+@pytest.mark.parametrize("notation, expected", [
+     ('a1', Position(0, 0)),
+     ('h8', Position(7, 7)),
+     ('f6', Position(5, 5)),
+     ('d3', Position(3, 2))
+     ])
+
+def test_from_algebraic(notation, expected):
+    assert Position.from_algebraic(notation) == expected
